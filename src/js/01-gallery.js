44d2b186cd521778be6d8dd89c4c3953d0ed3a9e
Sychov-Yurii <sychov.yurii@outlook.com>
2023-10-10 16:28:46 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -14,27 +14,5 @@ const markup = galleryItems.map(({ preview, original, description }) => {
 }).join('');
 
 refs.insertAdjacentHTML("beforeend", markup);
-refs.addEventListener('click', handlerClick); 
 
-function handlerClick(evt){
-    evt.preventDefault();
-    
-    if (!evt.target.classList.contains('gallery__image')) {
-     return   
-    }
-    const imageDescription = evt.target.alt;
-    const galleryItem = galleryItems.find(({description}) => description === imageDescription);
-
-    const lightbox = new SimpleLightbox(`
-    <div class="gallery__image">
-        <img src="${galleryItem.original}" width="900" alt="${galleryItem.description}">
-    </div>`);
-
-lightbox.open();
-document.addEventListener("keydown", handlerKey)
-function handlerKey(evt) {
-    if (evt.code === 'Escape') {
-     lightbox.close();
-    }
- }
-}
+const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,14 +3,13 @@ const STORAGE_KEY = 'feedback-form-state';
 
 const formData = {}
 const refs = {
-    form: document.querySelector('.js-feedback-form'),
-    textarea: document.querySelector('.js-feedback-form textarea'),
+    form: document.querySelector('.feedback-form'),
 };
 refs.form.addEventListener('submit', omFormSubmit);
 
 refs.form.addEventListener('input', e => {
     formData[e.target.name] = e.target.value;
-    console.log(formData);
+    
 });
 populateTextarea();
 function omFormSubmit(evt) {
