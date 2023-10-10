615e815b014b570480dee4acc5c1584b6a695e8b
Sychov-Yurii <125128096+Sychov-Yurii@users.noreply.github.com>
2023-10-10 14:55:29 +0000

    Deploying to gh-pages from @ Sychov-Yurii/goit-js-hw-08@f72199c6b5c4e81c46105336e20250979f64b71f 🚀

---
+++ 03-feedback.686e237c.js
@@ -0,0 +1,2 @@
+refs.form.addEventListener("input",(e=>{formData[e.target.name]=e.target.value})),localStorage.getItem(STORAGE_KEY)&&refs.textarea.value;
+//# sourceMappingURL=03-feedback.686e237c.js.map
